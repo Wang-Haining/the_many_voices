33e4cbd646f63d26b526cd547598c1f8e36d61c1
hw <whn395436129@gmail.com>
2023-03-22 12:18:27 -0400

    Add vertorize function

--- utils.py
+++ utils.py
@@ -89,3 +89,60 @@ def remove_whitespace(text: str) -> str:
         text with whitespaces removed
     """
     return re.sub(r"\s+", "", re.sub(r"\[([A-Za-z ]+)\]", "", text))
+
+
+def count_frequency(function_ngrams: List[str], text: str) -> List[int]:
+    """
+    Count frequencies of Chinese function uni/bi/tri/4-grams from a text. A subordinate gram will not be counted twice
+    if it is a part of a superior gram. For example, in "这是一个例子，用来展示在数'是'和'而是'时的区别。", we count "是" twice
+    (not thrice) and "而是" once.
+
+    Args:
+      function_ngrams: list of str, a function ngram list with no duplication
+      text: str, a document whose function ngram distribution is of interest
+
+    Return:
+      list of int, count of the corresponding ngram wrt `function_ngrams`.
+
+    Example:
+      ```python
+      > count_frequency(['几', '几几乎', '几几几乎'], "几几几乎几几几乎几几几个个几几乎")
+      > [3, 1, 2]
+      ```
+    """
+    # check duplicates
+    if len(set(function_ngrams)) != len(function_ngrams):
+        raise "Remove duplicates in `function_ngrams`."
+    raw_count_dict = {g: len(re.findall(g, text)) for g in function_ngrams}
+
+    adjusted_count_dict = {}
+    max_n = max([len(gram) for gram in function_ngrams])
+    for n in range(1, max_n + 1)[::-1]:  # tri/bi/uni/4-grams
+        if n == max_n:
+            adjusted_count_dict.update(
+                {
+                    gram: count
+                    for gram, count in raw_count_dict.items()
+                    if len(gram) == n
+                }
+            )
+        else:
+            longer_than_n_adjusted_count_dict_tmp = {
+                gram: count
+                for gram, count in adjusted_count_dict.items()
+                if len(gram) > n
+            }
+            for gram in [gram for gram in function_ngrams if len(gram) == n]:
+                deduct_count = 0
+                for longer_gram in longer_than_n_adjusted_count_dict_tmp.keys():
+                    if gram in longer_gram:
+                        multiplier = len(re.findall(gram, longer_gram))
+                        deduct_count += (
+                            longer_than_n_adjusted_count_dict_tmp.get(longer_gram)
+                            * multiplier
+                        )
+                adjusted_count_dict.update(
+                    {gram: raw_count_dict.get(gram) - deduct_count}
+                )
+
+    return [adjusted_count_dict.get(g) for g in function_ngrams]
